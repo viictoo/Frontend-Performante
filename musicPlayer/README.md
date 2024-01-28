5e37052654e802824180d500f2f8145f5bc5ed0e
Victor Langat <provicml@gmail.com>
2024-01-28 11:27:38 +0300

    updated scrubber and added README

---
+++ musicPlayer/README.md
@@ -0,0 +1,16 @@
+## Music Player
+
+A Simple Responsive Music Player with HTML CSS & Javascript
+
+**Features**
+<ul>
+<li>play</li>
+<li>pause</li>
+<li>previous</li>
+<li>next</li>
+<li>scrub</li>
+<li>timer</li>
+</ul>
+
+![Music Player](https://github.com/viictoo/Frontend-Performante/blob/master/musicPlayer/img/musicPlayer.png)
+
--- musicPlayer/script.js
+++ musicPlayer/script.js
@@ -10,6 +10,7 @@ const durationEl = document.getElementById('duration');
 const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
 
+
 let isPlaying = false
 
 // music files
@@ -21,22 +22,33 @@ const songs = [
   },
   {
     name: 'jacinto-2',
-    displayName: 'Seven Nation Army',
-    artist: 'ZAKES',
+    displayName: 'Kuja Utanipata',
+    artist: 'Qwangu',
   },
   {
     name: 'jacinto-3',
     displayName: 'Not Another Love Song',
     artist: 'Bass Singer',
+  },
+  {
+    name: 'metric-1',
+    displayName: 'Kuja Utanipata',
+    artist: 'Qwangu',
   }
 ]
 
+// toggle background glow
+function showBGGlow(key) {
+  if (key === 'on') art.style.boxShadow = '0 5px 30px 5px #f36d5956'
+  else art.style.boxShadow = 'none';
+}
 // play song
 function playSong() {
   isPlaying = true;
   playBtn.classList.replace('fa-play', 'fa-pause');
   playBtn.setAttribute('title', 'pause')
   music.play();
+  showBGGlow('on');
 }
 
 // pause song
@@ -45,21 +57,24 @@ function pauseSong() {
   playBtn.classList.replace('fa-pause', 'fa-play');
   playBtn.setAttribute('title', 'play')
   music.pause();
+  showBGGlow('off');
 }
 
 // toggle play pause song
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+document.addEventListener('keypress', (e) => {
+  if (e.key === " ") isPlaying ? pauseSong() : playSong()
+});
 
 // Update Player
 function loadSong(song) {
-  title.textContent = song.displayName;
-  artist.textContent = song.artist;
+  title.innerText = song.displayName;
+  artist.innerText = song.artist;
   music.src = `music/${song.name}.mp3`;
   art.src = `img/${song.name}.jpg`;
 }
 
 let songIndex = 0
-
 
 // nextSong
 function nextSong() {
@@ -83,30 +98,29 @@ function prevSong() {
 
 loadSong(songs[songIndex])
 
-// Update Progress Bar & Time
+// Update Progress Bar
 function updateProgressBar(e) {
   if (isPlaying) {
     const { duration, currentTime } = e.srcElement;
-    // Update progress bar width
-    const progressPercent = (currentTime / duration) * 100;
-    progress.style.width = `${progressPercent}%`;
-    // Calculate display for duration
-    const durationMinutes = Math.floor(duration / 60);
-    let durationSeconds = Math.floor(duration % 60);
-    if (durationSeconds < 10) {
-      durationSeconds = `0${durationSeconds}`;
-    }
-    // Delay switching duration Element to avoid NaN
-    if (durationSeconds) {
-      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
-    }
-    // Calculate display for currentTime
-    const currentMinutes = Math.floor(currentTime / 60);
-    let currentSeconds = Math.floor(currentTime % 60);
-    if (currentSeconds < 10) {
-      currentSeconds = `0${currentSeconds}`;
-    }
-    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+
+    // Update Progress Bar
+    const percentProgress = (currentTime / duration) * 100;
+    progress.style.width = `${percentProgress}%`;
+
+    // calculate and format duration
+    const durationMins = Math.floor(duration / 60);
+    let durationSecs = (Math.floor(duration % 60));
+    if (durationSecs < 10) durationSecs = `0${durationSecs}`;
+
+    // delay updating Duration text until value is ready to avoid NaN
+    if (durationSecs) durationEl.innerText = `${durationMins}:${durationSecs}`;
+
+    // calculate and format current time
+    const currentMins = Math.floor(currentTime / 60);
+    let currentSecs = (Math.floor(currentTime % 60));
+    if (currentSecs < 10) currentSecs = `0${currentSecs}`;
+    // delay updating Duration text until value is ready to avoid NaN
+    if (currentSecs) currentTimeEl.innerHTML = `${currentMins}:${currentSecs}`;
   }
 }
 
@@ -118,9 +132,12 @@ function setProgressBar(e) {
   music.currentTime = (clickX / width) * duration;
 }
 
-
 // Toggle Song Event Listeners
-prevBtn.addEventListener('click', prevSong)
-nextBtn.addEventListener('click', nextSong)
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
 progressContainer.addEventListener('click', setProgressBar);
+volumeControl.addEventListener('input', (event) => {
+  music.volume = event.target.value;
+ });
